1397b2a91468fe4beadf1f0b25ac1537a30dc7c3
michelangelo <michelangelo@oldair.homenet.telecomitalia.it>
2023-11-11 18:48:28 +0100

    progress

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,57 @@
+/*global console*/
 /* jshint esversion: 6 */
+/* jshint browser: true */
+
 // Import our custom CSS
 import '../scss/styles.scss';
 
 // Import all of Bootstrap's JS
 import * as bootstrap from 'bootstrap';
+import MicroModal from 'micromodal';
+
+import $ from "cash-dom";
+
+const data =
+{
+    crazed:
+    {
+        store: '#',
+        online: '#',
+        source: '#'
+    },
+    side:
+    {
+        store: '#',
+        online: '#',
+        source: '#'
+    },
+    cobra:
+    {
+        store: '#',
+        online: '#',
+        source: '#'
+    },
+    ladies:
+    {
+        store: '#',
+        online: '#',
+        source: '#'
+    },
+};
+
+const Controller =
+{
+    init_ui: evt =>
+    {
+        MicroModal.init();
+        $(document).on('click', 'button.js-store', evt =>
+        {
+            MicroModal.show('modal-1');
+        });
+    }
+};
+
+$(function () {
+    $('html').addClass('dom-loaded');
+    Controller.init_ui();
+});
